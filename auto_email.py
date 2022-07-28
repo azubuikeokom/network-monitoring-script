467789375dee96a7719d92dc59e3a1698334b72f
Azubuke <azubuikeokom@gmail.com>
2022-07-28 10:57:50 +0100

    added .env file to local directory

--- auto_email.py
+++ auto_email.py
@@ -1,14 +1,17 @@
 import ezgmail
 from netmiko import ConnectHandler
 import json
-import socket
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv() 
 def ping_test(link_maps):
     reachable=set()
     not_reachable=set()
     for ip in link_maps.keys():
         #send ping from router to ip 
-        output = device.send_command("ping -c 4 -m 10 %s" % ip)  
+        output = device.send_command("ping -c 4 -m 10 %s" % ip) 
         if "100.00%" not in output:
             reachable.add(link_maps[ip])
         else:
@@ -35,18 +38,17 @@ def config_link(hosts,links):
 """program starts here"""
 #connect to router via ssh
 try:
-    device = ConnectHandler(device_type='huawei', ip='x.x.x.x', username='jon_doe', password='don_joe') 
+    device = ConnectHandler(device_type='huawei', ip=os.getenv('public_ip'), username=os.getenv('myusername'), password=os.getenv('public_password')) 
 except:
     f=open('connection_log.txt','a')
-    f.write("Couldn't connect to router at x.x.x.x")
+    f.write("\nCouldn't connect to router at {} at {} ".format(datetime.now(),os.getenv('public_ip')) )
     f.close()
-    device = ConnectHandler(device_type='huawei', ip='x.x.x.x', username='jon_doe', password='don_joe')
+    device = ConnectHandler(device_type='huawei', ip=os.getenv('private_ip'), username=os.getenv('myusername'), password=os.getenv('private_password'))
 device.send_command("n")    
-hosts=['10.98.70.4','10.98.65.87','10.98.65.97','10.98.23.172','10.98.65.182','10.98.12.120','10.98.12.121','10.98.65.178']
 links=json.load(open("router_links.json"))
 #run pings
 ping_test(links)
 device.disconnect()
 
 #ezgmail.init()
-print("program completed") 
+#print("program completed") 
